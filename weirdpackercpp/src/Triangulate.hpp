3467e91aae1586ea188da35afee323bddc57e488
Steve132 <steve@soapforge.com>
2020-01-31 00:12:27 -0500

    Triangle intersection test

--- weirdpackercpp/src/Triangulate.hpp
+++ weirdpackercpp/src/Triangulate.hpp
@@ -11,38 +11,42 @@ std::vector<Triangle> triangulate(const Shape& shape);
 
 namespace {
 
-inline bool crossIntersect(const Triangle& t,const Point& b,const Point& c,float normal)
+inline bool crossIntersect(const Triangle& points,const Triangle& tri)
 {
-	Point cbr(c.y()-b.y(),-(c.x()-b.y()));
-	cbr*=normal;
-	float bcbr=b.dot(cbr);
-	return ((t[0].dot(cbr)-bcbr) < 0) && ((t[1].dot(cbr)-bcbr) < 0) && ((t[2].dot(cbr)-bcbr) < 0);
-}
-inline float computeNormal(const Triangle& t)
-{
-	Point ba=t[1]-t[0];
-	Point ca=t[2]-t[0];
-	ca=Point(ca.y(),-ca.x());
-	return ba.dot(ca);
+	Eigen::Matrix<float,3,2> dABC;
+	dABC << (points[0]-tri[2]).transpose(),
+			(points[1]-tri[2]).transpose(),
+			(points[2]-tri[2]).transpose();
+
+	Eigen::Vector2f d12=tri[1]-tri[2];
+	Eigen::Vector2f d20=tri[2]-tri[0];
+      
+    float D = d12.y() * d20.x() - d12.x() * d20.y();
+    D=-D;
+
+    Eigen::Vector3f s = dABC.leftCols<1>()*d12.y() - dABC.rightCols<1>()*d12.x();
+    Eigen::Vector3f t = dABC.leftCols<1>()*d20.y() - dABC.rightCols<1>()*d20.x();
+    
+    if(D < 0.0)
+    {
+        s=-s;
+        t=-t;
+        D=-D;
+    }
+
+    return (s.array() <= 0.0f).all()
+        || (t.array() <= 0.0f).all()
+        || ((s+t).array() >= D).all();
 }
 // test if one of the triangles has a side with all of the other triangles points on the outside.  Assume that we know all the normals of ech triangle, which tells us the direction that the points are specified.
-inline bool triangleIntersect3(const Triangle& t0,const Triangle& t1)
+inline bool triangleIntersect(const Triangle& t0,const Triangle& t1)
 {
-	float normal0 = computeNormal(t0);
-	float normal1 = computeNormal(t1);
-	
-	return 
-	crossIntersect(t1, t0[0], t0[1], normal0) &&
-	crossIntersect(t1, t0[1], t0[2], normal0) &&
-	crossIntersect(t1, t0[2], t0[0], normal0) &&
-	crossIntersect(t0, t1[0], t1[1], normal1) &&
-	crossIntersect(t0, t1[1], t1[2], normal1) &&
-	crossIntersect(t0, t1[2], t1[0], normal1);
+	!(crossIntersect(t0,t1) || crossIntersect(t1,t0));
 }
 }
 inline bool intersect(const Triangle& t1,const Triangle& t2)
 {
-		return triangleIntersect3(t1,t2);
+		return triangleIntersect(t1,t2);
 }
 
 
